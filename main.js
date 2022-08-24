f7ab3f1b8f74bb1eba80e42f305abe45160e359c
jessvoiture <jessicascarr989@gmail.com>
2022-08-23 21:10:52 -0400

    working prototype

--- main.js
+++ main.js
@@ -1,96 +1,90 @@
-// set up
+const margin = { top: 20, bottom: 40, left: 80, right: 20 };
 
-const margin = { top: 20, bottom: 20, left: 20, right: 20 };
+const width = 1300 - margin.left - margin.right;
+const height = 500 - margin.top - margin.bottom;
 
-const width = 675 - margin.left - margin.right;
-const height = 200 - margin.top - margin.bottom;
+const padding = 0.05
 
 const svg = d3.select('.viz')
-  .append('svg')
+    .append('svg')
     .attr('width', width + margin.left + margin.right)
     .attr('height', height + margin.top + margin.bottom)
-  .append('g')
+    .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
 
+d3.csv("https://raw.githubusercontent.com/jessvoiture/sound_week/main/datasets/sound_patterns_real.csv?version=123", d3.autotype).then(function(data){
 
-// create different groups for the top and bottom chart
+    // FORMAT THE DATES
+    // format time as 24 hour clock
+    var parseTime = d3.timeParse("%H:%M");
+    var formatTime = d3.timeFormat("%H:%M")
 
-const paddingBetweenCharts = 20;
-const chartHeight = (height / 2) - paddingBetweenCharts;
+    // parse time column
+    data.forEach(function(d) {
+        d.Time = parseTime(d.Time);
+        d.Vol = +d.Vol;
+    });
 
-const topChart = svg.append('g');
-const bottomChart = svg.append('g')
-    .attr('transform', `translate(0,${(height / 2) + paddingBetweenCharts})`);
+    // VERIFYING DATA TYPES
+    // var variables = data.columns;
+    // variables.forEach(function(d) { console.log("typeof " + d + ": " + typeof(data[1][d]))})
 
-// data
+    // SET UP X SCALE
+    var x = d3.scaleTime()
+        .domain([parseTime('00:00'), parseTime('23:45')])
+        .range([0, width]);
 
-const data = [
-  { x: 'TC11', y: 2, },
-  { x: 'TC10', y: 3, },
-  { x: 'TC9', y: 2, },
-  { x: 'TC8', y: 1.5, },
-  { x: 'TC7', y: 2, },
-  { x: 'TC6', y: 2, },
-  { x: 'TC5', y: 3, },
-  { x: 'TC4', y: 1, },
-  { x: 'TC3', y: 2, },
-  { x: 'TC2', y: 1.5, },
-  { x: 'TC1', y: 2, },
-];
+    // append x axis to svg
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x)
+             .ticks(d3.timeMinute.every(60))
+             .tickFormat(d3.timeFormat("%H")))
 
-// scales
+    // SET UP Y SCALE
+    var y = d3.scaleBand()
+        .range([ 0, height ])
+        .domain(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+        .padding(padding);     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
+ 
+    // append y axis to svg
+    svg.append("g")
+        .call( d3.axisLeft(y) );
 
-const x = d3.scalePoint()
-    .domain(["TC11", "TC10", "TC9", "TC8", "TC7", "TC6", "TC5", "TC4", "TC3", "TC2", "TC1"])
-    .range([0, width]);
+    var histogram = d3.histogram()
+        .domain(x.domain())
+        .thresholds(x.ticks(48)) // 24 hr x (2 * 30 min) / hr = 48
+        .value(d => d.Vol);
 
-const y = d3.scaleLinear()
-    .domain([0, d3.max(data, d =>  d.y)])
-    .range([chartHeight, 0]);
+    const maxNum = d3.max(data, function(d) { return d.Vol; });
 
-// mirror y scale by flipping range
-const yMirrored = y.copy().range([0, chartHeight]);
+    var yNum = d3.scaleLinear()
+        .range([0, y.bandwidth()])
+        .domain([-maxNum,maxNum]);
 
+    const day_data = d3.group(data, d => d['Day of Week']);
+    var keys = Array.from(day_data.keys())
+    var values = Array.from(day_data.values())
+    svg
+        .selectAll("myViolin")
+        .data(values)
+        .enter()
+        .append("g")
+        .attr("transform", function(d, i){ 
+            return("translate(" + 0 + "," + (y(d[i]['Day of Week'])) + ")") 
+        })
+        .append("path")
+        // .datum(d => d[1])    // So now we are working bin per bin
+        .style("stroke", "none")
+        .style("fill","#69b3a2")
+        .attr("d", d3.area()
+            .y0(function(d, i){
+                return(yNum(-d.Vol))} )
+            .y1(function(d){ 
+                return(yNum(d.Vol)) } )
+            .x(function(d){ 
+                return(x(d.Time)) } )
+            .curve( d3.curveMonotoneY) // d3.curveCatmullRom.alpha(1)   // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
+            )
 
-// axis generators
-
-const xAxis = d3.axisBottom(x);
-const xAxisMirrored = d3.axisTop(x);
-
-
-// area generators
-
-const area = d3.area()
-    .x(d => x(d.x))
-    .y0(y(0))
-    .y1(d => y(d.y));
-
-const areaMirrored = d3.area()
-    .x(d => x(d.x))
-    .y0(d => yMirrored(d.y))
-    .y1(yMirrored(0));
-
-
-// top chart
-
-topChart.append("path")
-    .datum(data)
-    .style("fill", "#D04242")
-    .attr("d", areaMirrored);
-
-topChart.append("g")
-    .attr("class", "x axis")
-    .call(xAxisMirrored);
-
-
-// bottom chart
-
-bottomChart.append("path")
-    .datum(data)
-    .style("fill", "#D04242")
-    .attr("d", area);
-
-bottomChart.append("g")
-    .attr("class", "x axis")
-    .attr("transform", `translate(0,${chartHeight})`)
-    .call(xAxis);
+})
