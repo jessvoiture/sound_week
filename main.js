69c46706832fc88219d1050f984105942f2f3532
jessvoiture <jessicascarr989@gmail.com>
2022-08-26 12:48:22 -0400

    new chart

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const margin = { top: 20, bottom: 40, left: 80, right: 20 };
 const width = 700 - margin.left - margin.right;
 const height = 700 - margin.top - margin.bottom;
 
-const padding = 0.15
+const barSize = 50;
+
+const colScale = d3.scaleOrdinal()
+    .range(["#FF0000","#0D68FF","#8B0CE8", "#12c748"])
+    .domain(["Youtube", "TV/Film", "Podcast", "Music"]);
+
 
 const svg = d3.select('.viz')
     .append('svg')
@@ -12,82 +17,80 @@ const svg = d3.select('.viz')
     .append('g')
     .attr('transform', `translate(${margin.left},${margin.top})`);
 
-const graph_col = "#fbf7ff"
+d3.csv("https://raw.githubusercontent.com/jessvoiture/sound_week/main/datasets/sound_patterns_swimlanes.csv?version=123", d3.autotype).then(function(data){
 
-d3.csv("https://raw.githubusercontent.com/jessvoiture/sound_week/main/datasets/sound_patterns_real.csv?version=123", d3.autotype).then(function(data){
-
-    // FORMAT THE DATES
+    /* FORMAT DATA TYPES */
     // format time as 24 hour clock
     var parseTime = d3.timeParse("%H:%M");
     var formatTime = d3.timeFormat("%H:%M")
 
-    // parse time column
+    // correct data types
     data.forEach(function(d) {
-        d.Time = parseTime(d.Time);
+        d.Start = parseTime(d.Start);
+        d.Stop = parseTime(d.Stop);
         d.Vol = +d.Vol;
     });
+    
+    /* SCALES */
+    // x
+    var x = d3.scaleTime()		
+        .domain([0, 24]) // hours
+        .range([0, width]);
+    
+    // y    
+    var y = d3.scaleBand()
+        .domain(["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"])
+        .range([0, height]);
 
-    // VERIFYING DATA TYPES
-    // var variables = data.columns;
-    // variables.forEach(function(d) { console.log("typeof " + d + ": " + typeof(data[1][d]))})
+    // bind y axis to svg    
+    svg.append("g")
+        .call( d3.axisLeft(y) );
 
-    // SET UP X SCALE
-    var x = d3.scaleTime()
+    // twenty four hour scale    
+    var tfh = d3.scaleTime()
         .domain([parseTime('00:00'), parseTime('23:45')])
         .range([0, width]);
 
     // append x axis to svg
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x)
+        .call(d3.axisBottom(tfh)
              .ticks(d3.timeMinute.every(180))
              .tickFormat(d3.timeFormat("%H")))
-
-    // SET UP Y SCALE
-    var y = d3.scaleBand()
-        .range([ 0, height ])
-        .domain(["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
-        .padding(padding);     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
- 
-    // append y axis to svg
-    svg.append("g")
-        .call( d3.axisLeft(y) );
-
-    var histogram = d3.histogram()
-        .domain(x.domain())
-        .thresholds(2) // 24 hr x (2 * 30 min) / hr = 48
-        .value(d => d.Vol);
-
-    const maxNum = d3.max(data, function(d) { return d.Vol; });
-
-    var yNum = d3.scaleLinear()
-        .range([0, y.bandwidth()])
-        .domain([-maxNum,maxNum]);
-
-    const day_data = d3.group(data, d => d['Day of Week']);
-    var keys = Array.from(day_data.keys())
-    var values = Array.from(day_data.values())
-
+    
+    /* add bars to chart */
     svg
-        .selectAll("myViolin")
-        .data(values)
+        .selectAll("rect")
+        .data(data)
         .enter()
-        .append("g")
-        .attr("transform", function(d, i){ 
-            return("translate(" + 0 + "," + (y(d[i]['Day of Week'])) + ")") 
+        .append("rect")
+        .attr("class","sound_bar")
+        .attr("x", function(d){
+            return tfh(d.Start) 
         })
-        .append("path")
-        // .datum(d => d[1])    // So now we are working bin per bin
-        .style("stroke", "none")
-        .style("fill", graph_col)
-        .attr("d", d3.area()
-            .y0(function(d, i){
-                return(yNum(-d.Vol))} )
-            .y1(function(d){ 
-                return(yNum(d.Vol)) } )
-            .x(function(d){ 
-                return(x(d.Time)) } )
-            .curve( d3.curveMonotoneY) // d3.curveCatmullRom.alpha(1)   // This makes the line smoother to give the violin appearance. Try d3.curveStep to see the difference
-            )
+        .attr("y",function(d) { 
+            return y(d.Day_abbr) + 21}
+        )
+        .attr("width",function(d){
+            var hstart = d.Start,
+                hstop = d.Stop;
+                return x((hstop-hstart)/3600000);	//date operations return a timestamp in miliseconds, divide to convert to hours
+        })
+        .attr("height", barSize)
+        .attr("rx", bar_curve_decider)
+        .attr("ry", bar_curve_decider)
+        .style("fill", function(d) { return colScale(d.Medium) })
+        .style("opacity", function(d) { return d.Vol / 10})
+
+    function bar_curve_decider(d) {
+        if (d.Method == "Earphones") {
+            return 10;
+        } else if (d.Method == "Outloud")  {
+            return 4;
+        } else {
+            return 0;
+        }
+    }
+        
 
 })
